55cce15b3b1cc9aa6ea9d44ed9b8e5faca91f5e2
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-10-05 16:32:14 -0300

    test finish web_local

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title={"Speak"} onPress={() => { Speech.speak('Open up App.js to start working on your app!') } }/>
       <StatusBar style="auto" />
     </View>
   );
